e701975c0fe64f75bce088f8756a27278283644d
hyderarain <harain@chicagobooth.edu>
2021-08-18 14:12:36 +0000

    some updates

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,8 @@
 class BookmarksController < ApplicationController
   def index
-    matching_bookmarks = Bookmark.all
+    the_id = session.fetch("user_id")
+
+    matching_bookmarks = Bookmark.where({:user_id => the_id})
 
     @list_of_bookmarks = matching_bookmarks.order({ :created_at => :desc })
 
@@ -19,7 +21,7 @@ class BookmarksController < ApplicationController
 
   def create
     the_bookmark = Bookmark.new
-    the_bookmark.user_id = params.fetch("query_user_id")
+    the_bookmark.user_id = session.fetch("user_id")
     the_bookmark.movie_id = params.fetch("query_movie_id")
 
     if the_bookmark.valid?
